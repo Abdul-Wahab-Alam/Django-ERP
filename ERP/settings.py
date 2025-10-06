9a713e774bfcb7a114b81cbbbb1db44747cb78a3
Abdul-Wahab-Alam <abdulwahab96540@gmail.com>
2025-10-06 16:57:51 -0700

    changing database

--- ERP/settings.py
+++ ERP/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'ERP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_erp',
+        'USER': 'root',
+        'PASSWORD': 'abc123',
+        'HOST': '127.0.0.1',  # use IP instead of 'localhost'
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
