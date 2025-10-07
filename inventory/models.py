38aba3361e251ea4982b168b8a2e59aad2ce18ad
Abdul-Wahab-Alam <abdulwahab96540@gmail.com>
2025-10-06 19:01:53 -0700

    base django

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Inventory(models.Model):
+    product_id = models.AutoField(default=1)
+    product_name = models.CharField()
+
+    def __str__(self):
+        return self.product_name
